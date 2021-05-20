5cf4b90211a601ac0a946a30dd37a4c6b270427f
qihan <497061632@qq.com>
2021-05-20 18:33:36 +0800

    替换源改为字符串数组

--- Shell.sh
+++ Shell.sh
@@ -1,7 +1,11 @@
 #! /bin/bash
 
-sourceWord="a"
-targetWord="b"
+sourceWordArr=(
+    '<link href="https://fonts.googleapis.com/css?family=Roboto&amp;display=swap" rel="stylesheet">'
+    '<noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-5V25JL6" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>'
+    '<link href="https://fonts.googleapis.com/css?family=Roboto&amp;display=swap" rel="stylesheet" />'
+)
+targetWord=""
 
 #读取目录
 echo "输入要替换的目录"
@@ -9,6 +13,7 @@ read dir
 #读取后缀
 echo "输入要求的后缀"
 read suffix
+echo "########开始替换###########"
 
 #遍历读取文件函数
 function read_dir() {
@@ -29,7 +34,15 @@ function read_dir() {
 #替换逻辑
 function handle_file() {
     echo ${1}
-    sed -i "s/${sourceWord}/${targetWord}/g" ${1}
+
+    idx=0
+    while [[ $idx < ${#sourceWordArr[*]} ]]; do
+        #执行替换
+        sed -i "s#${sourceWordArr[${idx}]}#${targetWord}#g" ${1}
+
+        #自加
+        let "idx++"
+    done
 }
 
 read_dir $dir
